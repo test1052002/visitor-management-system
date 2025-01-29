f51480ce1693de1f094cf445aee2a5c3891c6bff
test <test1052002@gmail.com>
2025-01-29 16:51:02 +0530

    update server

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ const app = express();
 // Middleware
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://visitor-management-system-flg4.vercel.app'],
+}));
+
 app.use(express.json());
 
 // Serve static files from uploads directory
